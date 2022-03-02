a5f0eb21bd4f8f27b189bc593e7f8a4aaea0f2d3
Alexey Shmelev <alexsey_89@bk.ru>
2022-03-02 22:47:13 +0300

    start write parser

--- js/test.js
+++ js/test.js
@@ -1,6 +1,28 @@
 window.onload = function()
 {
 	
-	console.log('123');
+	console.log('Начинаем обработку');
+
+	// находимся на странице с темами
+	if (document.location.pathname.indexOf('/Prepare/Content/ShowThemes/') == 0)
+	{
+		// просто выведем список тем в консоль, потом подумаем как можно проходится по всем темам сразу
+		$("#themes-table").find('a').each(function()
+		{
+			//console.log($(this).html());
+		});
+	}
+	// находимся на странице с вопросами
+	if (document.location.pathname.indexOf('/Prepare/Content/ShowThemeQuestions') == 0)
+	{
+		var actions = $(".body").find("table").find("td.actions");
+
+		//console.log(actions);
+		// обновляем станицу
+		/*setTimeout(function(){
+			location.reload();
+		}, 1000);*/
+	}
+
 	
 }
