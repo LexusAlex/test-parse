711abae8d260f89ea4f40c8f8ba5810e6c3dba0f
Alexsey Shmelev <alexsey_89@bk.ru>
2022-03-03 13:43:56 +0300

    fix test

---
+++ js/content.js
@@ -0,0 +1,7 @@
+window.onload = function()
+{
+    //setTimeout(addBtn, 1000);
+    var jq = document.createElement('script');
+    jq.src = "https://code.jquery.com/jquery-3.4.1.min.js";
+    document.getElementsByTagName('head')[0].appendChild(jq);
+};
--- js/test.js
+++ js/test.js
@@ -1,6 +1,7 @@
+
 window.onload = function()
 {
-	
+
 	console.log('Начинаем обработку');
 
 	// находимся на странице с темами
@@ -9,20 +10,30 @@ window.onload = function()
 		// просто выведем список тем в консоль, потом подумаем как можно проходится по всем темам сразу
 		$("#themes-table").find('a').each(function()
 		{
-			//console.log($(this).html());
+			console.log($(this).html());
 		});
 	}
+
 	// находимся на странице с вопросами
 	if (document.location.pathname.indexOf('/Prepare/Content/ShowThemeQuestions') == 0)
 	{
-		var actions = $(".body").find("table").find("td.actions");
+		var actions = $(".body").find("table").find("td");
+		var childrens = document.querySelectorAll("#theme-questions tbody")[0].children;
+		for (var i = 0; i < childrens.length; i++) {
+			var item = childrens[i];
+			console.log(item);
+		}
 
-		//console.log(actions);
+		/*childrens.forEach(function( child ){
+			console.log(child);
+		});*/
+
+		console.log(document.querySelectorAll("#theme-questions tbody")[0].children); // выведет в консоли "Тестовая страница"
 		// обновляем станицу
 		/*setTimeout(function(){
 			location.reload();
 		}, 1000);*/
-	}
 
-	
+
+	}
 }
