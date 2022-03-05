cde6bb1316f216d2f92dc41da087cdde722e0f55
Alexsey Shmelev <alexsey_89@bk.ru>
2022-03-05 14:02:00 +0300

    fix

--- js/test.js
+++ js/test.js
@@ -14,29 +14,73 @@ window.onload = function()
 		});
 	}
 
-	// находимся на странице с вопросами
-	if (document.location.pathname.indexOf('/Prepare/Content/ShowThemeQuestions') == 0)
-	{
-		var actions = $(".body").find("table").find("td");
-		var tags = document.querySelectorAll(".block script")
-		/*for (var i = 0; i < childrens.length; i++) {
-			var item = childrens[i];
-			console.log(item);
-		}*/
+	//function handler() {
+		// находимся на странице с вопросами
+		if (document.location.pathname.indexOf('/Prepare/Content/ShowThemeQuestions') == 0) {
 
-		/*childrens.forEach(function( child ){
-			console.log(child);
-		});*/
+			var questions = $(".body").find("table").find("td.actions a.icon-info");
 
-		//console.log(); // выведет в консоли "Тестовая страница"
-		//console.log(JSON.stringify(tags[tags.length - 1].textContent, null, 4));
-		//console.log(viewModel);
-		//console.log(cell);
-		// обновляем станицу
-		/*setTimeout(function(){
-			location.reload();
-		}, 1000);*/
+			/*questions.first().click(function(){
+				console.log(123);
+			});*/
+
+			/*questions.each(function(i, e)
+			{
+				console.log(e.click());
+
+			});*/
+		    /*var	items_length = questions.length;
+			var i = 0;
+
+			questions.click(function(e){
+				e.preventDefault();
+				console.log($(this));
+			});
+
+			setInterval(function(){
+				if(i == items_length) {
+					i = 0
+				}
+				questions.eq(i).click();
+				//i++
+
+			}, 2000)
 
 
-	}
+
+
+			//---------------------------------------
+
+			//console.log(actions);
+			//var tags = document.querySelectorAll(".block script")
+			/*for (var i = 0; i < childrens.length; i++) {
+                var item = childrens[i];
+                console.log(item);
+            }*/
+
+			/*childrens.forEach(function( child ){
+                console.log(child);
+            });*/
+
+			//console.log(); // выведет в консоли "Тестовая страница"
+			//console.log(JSON.stringify(tags[tags.length - 1].textContent, null, 4));
+			//console.log(viewModel);
+			//console.log(cell);
+			// обновляем станицу
+			/*setTimeout(function(){
+                location.reload();
+            }, 1000);*/
+
+			/*var http = new XMLHttpRequest();
+            http.open('GET', document.location.href, true);  // составить асинхронный (по умолчанию true) GET запрос страницы
+            http.onreadystatechange = function () {  // обратная связь: отдаёт ответ на запрос
+                if (this.readyState == 4 && this.status == 200) {  // отследить момент, когда пришёл полный ответ
+                    console.log( this.responseText );  // this.responseText — ответ в виде текста
+                }
+            }
+            http.send();  // отправить запрос*!/*/
+
+		//console.log($(".body").find("table").find("td"));
+		}
+	//}
 }
